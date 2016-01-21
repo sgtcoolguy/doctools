41a8e39a03a2e26161352289fa87b7613118988f
Brian Immel <jawa9000@gmail.com>
2016-01-21 10:23:29 -0800

    Replaced $TI_ROOT with $WORKSPACE variable for working directories so this script can work with apps-docs Jenkins scripts.

--- get_confluence_auto.sh
+++ get_confluence_auto.sh
@@ -8,56 +8,56 @@
 
 DATE=$(date +%Y-%m-%d)
 CONFLUENCE_FILE=confluence_guide2-$DATE.zip
+WORKSPACE=$TI_ROOT/workspace
 date
 
 ## Create a series of potentially missing directories
 ## if the htmlguides directory is missing, add it back in
-if [ ! -d $TI_ROOT/doctools/htmlguides ]; then
+if [ ! -d $WORKSPACE/doctools/htmlguides ]; then
 	echo "htmlguide directory is missing. Creating that directory."
-	mkdir $TI_ROOT/doctools/htmlguides
+	mkdir $WORKSPACE/doctools/htmlguides
 fi
 ## this addition of the dist/platform and dist/arrowdb directories are not necessary for the creation of the htmlguides content but this directory is wiped out from the update_modules.sh script and needs to be added back in before the doc pub can finish.
-if [ ! -d $TI_ROOT/doctools/dist/platform ]; then
+if [ ! -d $WORKSPACE/doctools/dist/platform ]; then
 	echo "../dist/platform directory is missing. Creating that directory."
-	mkdir -p $TI_ROOT/doctools/dist/platform
+	mkdir -p $WORKSPACE/doctools/dist/platform
 fi
-if [ ! -d $TI_ROOT/doctools/dist/arrowdb ]; then
+if [ ! -d $WORKSPACE/doctools/dist/arrowdb ]; then
 	echo "../dist/arrowdb directory is missing. Creating that directory."
-	mkdir -p $TI_ROOT/doctools/dist/arrowdb
+	mkdir -p $WORKSPACE/doctools/dist/arrowdb
 fi
 ## if the Confluence_working is missing, create it
-if [ ! -d $TI_ROOT/Confluence_working ]; then
+if [ ! -d $WORKSPACE/Confluence_working ]; then
 	echo "../Confluence_working directory is missing. Create that directory."
-	mkdir $TI_ROOT/Confluence_working
+	mkdir $WORKSPACE/Confluence_working
 fi
 
+cd $WORKSPACE/Confluence_working
+pwd
+
+
+## **** something isn't working right here.
+## **** the downloaded .jar file isn't being saved anywhere.
 
 echo "Downloading today's guide2 .jar file."
 ## function to download guide2 confluence space
-CONFLUENCE_USERNAME='doc-robot'
-CONFLUENCE_PASSWORD='szE8U9OWiJmVsfuCEQCMAgYwHokRrkqowyixe5HLqwzPUJOziy'
-	REST_URL='https://wiki.appcelerator.org/rest/scroll-eclipsehelp/1.0/sync-export?exportSchemeId=guides2-7F0000010146EBA9ABDB43844FCF8B50&rootPageId=29004729'
-wget --content-disposition "$REST_URL&os_username=$CONFLUENCE_USERNAME&os_password=$CONFLUENCE_PASSWORD"
+#CONFLUENCE_USERNAME='doc-robot'
+#CONFLUENCE_PASSWORD='szE8U9OWiJmVsfuCEQCMAgYwHokRrkqowyixe5HLqwzPUJOziy'
+	#REST_URL='https://wiki.appcelerator.org/rest/scroll-eclipsehelp/1.0/sync-export?exportSchemeId=guides2-7F0000010146EBA9ABDB43844FCF8B50&rootPageId=29004729'
+#wget --content-disposition "$REST_URL&os_username=$CONFLUENCE_USERNAME&os_password=$CONFLUENCE_PASSWORD"
+#touch com.something.jar
 ## rename the downloaded .jar file to include the a human readable name
 mv com.*.jar $CONFLUENCE_FILE
-## If the working Confluence directory doesn't exist, create it
-if [ -d $TI_ROOT/Confluence_working ]; then
-	cd $TI_ROOT
-	mkdir Confluence_working
-	cd -
-fi
-## move .jar file to working directory
-mv $CONFLUENCE_FILE $TI_ROOT/Confluence_working
 
 date
 ## detect if the htmlguides directory is populated or not
-cd $TI_ROOT/doctools/htmlguides
-current=${PWD}
-if [ "$(ls -A $TI_ROOT/doctools/htmlguides)" ]; then
-	echo "Emptying $current"
-	rm -r *
-	echo "Unzipping $TI_ROOT/Confluence_working/$CONFLUENCE_FILE in $current"
-	unzip -o $TI_ROOT/Confluence_working/$CONFLUENCE_FILE
-	date		
-fi
+#cd $TI_ROOT/doctools/htmlguides
+#current=${PWD}
+#if [ "$(ls -A $TI_ROOT/doctools/htmlguides)" ]; then
+#	echo "Emptying $current"
+#	rm -r *
+#	echo "Unzipping $TI_ROOT/Confluence_working/$CONFLUENCE_FILE in $current"
+#	unzip -o $TI_ROOT/Confluence_working/$CONFLUENCE_FILE
+#	date		
+#fi
 
