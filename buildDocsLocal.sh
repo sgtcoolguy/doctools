945c35d243a97483d4cccca0dda233c4908d78ef
Brian Immel <jawa9000@gmail.com>
2016-03-17 17:02:58 -0700

    Created or updated doc pub scripts
    Created or updated a few doc pub scripts that I use to locally build
    and test the documentation prior to publication.

---
+++ buildDocsLocal.sh
@@ -0,0 +1,52 @@
+#####################################################################
+## Purpose: semi-automate the documentation build process locally  ##
+##																   ##
+## For more information visit 									   ##
+## ?															   ##
+#####################################################################
+
+
+
+
+## empty ../platform directory
+if [ -d $TI_ROOT/doctools/dist/platform ]; then
+	echo "Emptying ../dist/platform directory."
+	cd $TI_ROOT/doctools/dist/platform
+	rm -r *	
+fi
+
+
+## empty ../arrowdb directory
+if [ -d $TI_ROOT/doctools/dist/arrowdb ]; then
+	echo "Emptying ../dist/arrowdb directory."
+	cd $TI_ROOT/doctools/dist/arrowdb
+	rm -r *
+fi
+
+
+## empty the output directory of the guides
+##if [ -d $TI_ROOT/doctools/htmlguides ]; then
+	##echo "Emptying ../htmlguides directory."
+	##cd $TI_ROOT/doctools/htmlguides
+	##rm -r *
+##fi
+
+## run through the basic scripts to build the docs locally
+cd $TI_ROOT/doctools
+sh deploy.sh prod > messages.txt
+sh clouddeploy.sh >> messages.txt
+sh deploy.sh -o arrow -o alloy -o modules -s prod >> messages.txt
+sh clouddeploy.sh -s prod >> messages.txt
+bash clouddeploy.sh prod >> messages.txt
+bash build_platform.sh >> messages.txt
+cd $TI_ROOT/appc_web_docs
+bash ../doctools/copy_platform.sh >> messages.txt
+bash ../doctools/copy_cloud.sh >> messages.txt
+
+## open the message.txt file and you need to manually search for error messages
+open -a TextWrangler messages.txt
+
+## open localhost and manually review the pages
+open http://localhost
+echo "Manually check the page(s) you updated."
+
