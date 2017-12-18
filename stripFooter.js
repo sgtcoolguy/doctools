abce1bb123d90b47d68122b818db421532a41f4e
Brian Immel <jawa9000@gmail.com>
2017-12-18 08:58:58 -0800

    Revised the stripFooter.js script

--- stripFooter.js
+++ stripFooter.js
@@ -5,32 +5,12 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 var shell = require('shelljs');
-var fileNames = shell.ls('htmlguides/*.html'); // get an array of all files that end with .html in the htmlguides directory
 
-for (i in fileNames) {
-	$ = cheerio.load(fs.readFileSync(fileNames[i]).toString()); // read in HTML file with jQuery-like features
-	if ($('div.footer').length > 0) { // if div.footer is found, remove it
-			console.log("Removing footer from ../" + fileNames[i]);
-			$('div.footer').remove(); // remove footer
-	} else {
-		console.log(fileNames[i] + " has no div.footer element.");
-	}
-
-	var removed = $.html(); // updated html
-	fs.writeFileSync(fileNames[i],removed); // save out HTML file
-}
-
-// ** There is a bug in this program: regardless of the last file in the fileNames array, the program will throw an error (see below). The program still executes as it should and the div.footer element is removed regardless.
-// ** this can be fixed by using the map method instead of the for loop
-/*
-		at Error (native)
-    at Object.fs.openSync (fs.js:584:18)
-    at Object.fs.readFileSync (fs.js:431:33)
-    at Object.<anonymous> (/Users/bimmel/Documents/Repositories/doctools/stripFooter.js:12:22)
-    at Module._compile (module.js:413:34)
-    at Object.Module._extensions..js (module.js:422:10)
-    at Module.load (module.js:357:32)
-    at Function.Module._load (module.js:314:12)
-    at Function.Module.runMain (module.js:447:10)
-    at startup (node.js:142:18)
-*/
+shell.ls('htmlguides/*.html').map(function(file) {
+  $ = cheerio.load(fs.readFileSync(file).toString());
+  if ($('div.footer').length > 0) {
+		console.log("Removing footer element from ../" + fileNames[i]);
+		$('div.footer').remove();
+    fs.writeFileSync(file,$.html());
+  }
+});
