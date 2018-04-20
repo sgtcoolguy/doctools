2cc9c0a565f2de1c199884d51768390381ef5c2d
Brian Immel <jawa9000@gmail.com>
2018-04-20 08:25:01 -0700

    Init commit
    Script to copy fixes.js into the doc build prior to publication

--- js_fix.sh
+++ js_fix.sh
@@ -1,4 +1,2 @@
-##
-## /Users/bimmel/Documents/Repositories/appc_web_docs/platform/latest/resources/js
 mkdir $TI_ROOT/appc_web_docs/platform/latest/resources/js
 cp js_fix/fixes.js $TI_ROOT/appc_web_docs/platform/latest/resources/js
