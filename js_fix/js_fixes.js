b0370efce5b390a219d583313c5a40284d5bdbe4
Brian Immel <jawa9000@gmail.com>
2018-06-13 13:48:01 -0700

    renamed fixes.js to js_fixes.js

--- js_fix/js_fixes.js
+++ js_fix/js_fixes.js
@@ -1,5 +1,6 @@
-// Fix for close buttons on tabs. See https://jira.appcelerator.org/browse/TIDOC-3141
+
 $(document).ready(function() {
+  // Fix for close buttons on tabs. See https://jira.appcelerator.org/browse/TIDOC-3141
   $('div.r a.icon-cancel-1.close').each(function() {
       $(this).text('x');
       $(this).css('text-align','center');
