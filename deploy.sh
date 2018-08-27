3272b5c9b90052dab6ba8909c200535fa265553c
Brian Immel <jawa9000@gmail.com>
2018-08-27 03:17:36 -0700

    Added titanium-web-dialog to module list

--- deploy.sh
+++ deploy.sh
@@ -196,7 +196,8 @@ if [ $include_modules ]; then
     module_dirs+=" $APPC_MODULES/ti.geofence/apidoc
       		         $APPC_MODULES/appcelerator.https/apidoc
                    $APPC_MODULES/com.appcelerator.apm/apidoc
-                   $APPC_MODULES/appcelerator.encrypteddatabase"
+                   $APPC_MODULES/appcelerator.encrypteddatabase
+                   $APPC_MODULES/titanium-web-dialog"
 fi
 
 if [ -d "$TI_ROOT/titanium_mobile_windows" ]; then
