4a1b06795773008fe0ff800bb78befe8dbe987b5
Brian Immel <jawa9000@gmail.com>
2018-12-13 08:47:13 -0800

    SDK 7.5.1.RC and CLI 7.0.9.RC

--- js_fix/js_fixes.js
+++ js_fix/js_fixes.js
@@ -1,4 +1,9 @@
-
+if (window.location.href.indexOf('?mobile=') > -1) { // if the URL contains a mobile version, add a noindex meta element to the document
+  var meta = document.createElement('meta');
+  meta.name = 'robots';
+  meta.content = 'noindex';
+  document.getElementsByTagName('head')[0].appendChild(meta);
+}
 $(document).ready(function() {
   // Fix for close buttons on tabs. See https://jira.appcelerator.org/browse/TIDOC-3141
   $('div.r a.icon-cancel-1.close').each(function() {
