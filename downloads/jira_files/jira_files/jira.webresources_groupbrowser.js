b8997ef1492e65ee63c922e4e5fe1ab96310d0e8
Brian Immel <jawa9000@gmail.com>
2018-12-19 15:27:02 -0800

    CLI 7.0.9.GA and SDK 7.4.2.GA releases

--- downloads/jira_files/jira_files/jira.webresources_groupbrowser.js
+++
@@ -1,3 +0,0 @@
-;
-/* module-key = 'jira.webresources:groupbrowser', location = '/includes/jira/admin/group-browser/group-browser.js' */
-require(["jquery","jira/admin/group-browser/group-label-lozenge"],function($){$(function(){$(".operations-list .aui-button[disabled]").tooltip({gravity:"e",html:false})})});;
