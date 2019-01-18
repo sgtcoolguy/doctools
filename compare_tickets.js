fdfb88d3beb34c600d641297fb6d817423d87e3a
Brian Immel <jawa9000@gmail.com>
2019-01-18 10:55:14 -0800

    Update compare_tickets.js
    Re-wrote this script so it scrapes the Confluence release note and Jira release board automatically (based on user supplied arguments) instead of waiting for the user to download the content manually.

--- compare_tickets.js
+++ compare_tickets.js
@@ -1,45 +1,146 @@
-// Purpose: to generate a list of missing Jira tickets between two HTML documents (wiki release note and Jira release board). See https://wiki.appcelerator.org/x/fhhbAw for details.
+/*
+  Purpose: Generate a list of missing tickets from both the release note (wiki page) and Jira release board so the writer can see what was added and/or removed from the release after the draft of the release note has been written. See https://wiki.appcelerator.org/x/fhhbAw for details.
+  Usage: node compare_tickets.js -u <Atlasssian username> -p <Atlasssian password> -j <URL for Jira release board> -c <URL for release note in Confluence>
+  Note: This script assumes the username and password for both Confluence and Jira are the same.
+*/
 
-const fs = require('fs');
+const Nightmare = require("nightmare");
 const cheerio = require('cheerio');
-const shell = require('shelljs');
-var wikifile = process.argv[2]; // HTML document for wiki release note: './downloads/wiki.html'
-var jirafile = process.argv[3]; // HTML document for Jira release board: './downloads/jira.html'
+const request = require('request');
+const nightmare = Nightmare({
+    show: true // use false if you don't want to see Electron "jumping through all the hoops"
+});
+const program = require('commander');
+const jiraSelector = 'section.aui-page-panel-content'; // selector of where the list of Jira tickets can be found
+const confluenceSelector = '#main-content'; // selector of where the list of Confluence tickets can be found
 
-var jiraArray = []; // array to hold all Jira ticket links found in a Jira release board
-var wikiJiraArray = []; // array to hold all Jira ticket links found in a release note
+program // set up flags
+  .version('0.0.1')
+  .usage('-u <username> -p <password> -j <Jira url> -c <Confluence url> -d <milliseconds>')
+  .option('-u, --user', '*required* Username id')
+  .option('-p, --password', '*required* User\'s password')
+  .option('-j, --jira', '*required* URL for Jira release board')
+  .option('-c, --confluence', '*required* URL for Confluence release note')
+  .option('-d, --delay', 'Delay (in milliseconds) to wait for server response')
+  .parse(process.argv);
 
-if (wikifile && jirafile) {
-  getList('ul li a', wikifile, wikiJiraArray);
-  getList('div#page section#content div div section ul li a', jirafile, jiraArray);
+async function run() { // execute the functions in proper order
+  var arguments = {}; // object to hold the entered arguments
+  var confluenceList = []; // array to hold a list of tickets found in the release note
+  var jiraList = []; // array to hold a list of tickets found in the Jira release board
 
-  reportDiff(wikiJiraArray, jiraArray, 'Jira'); // report on unmatched tickets in the Jira release board
-  reportDiff(jiraArray, wikiJiraArray, 'wiki'); // report on unmatched tickets in the release note
-} else {
-  console.log('You need to supply a path to either the wiki release note or Jira release board HTML docuement(s).\nQuitting app.');
-  process.exitCode = 1;
-}
-
-
-
-function getList(linkLocation, file, array) { // find all Jira links within a file
-  $ = cheerio.load(fs.readFileSync(file).toString()); // read file in
-
-  $(linkLocation).each(function() { // find all links
-    if ($(this).attr('href').indexOf('jira.') > -1) { // if the link uses 'jira.' as part of it's URL, assume it's a Jira ticket link
-      array.push($(this).text()); // push the link into an array
+  for (var i = 0; i < program.rawArgs.length; i++) { // loop through the array of raw arguments to gather the entered arguments
+    if (program.rawArgs[i] == '--user' || program.rawArgs[i] == '-u') {
+      arguments.user = program.rawArgs[i + 1];
     }
-  });
-}
-
-function reportDiff(array1, array2, listName) { // report the differences between two lists of links from two different files
-  let difference = array1.filter(x => !array2.includes(x)); // find the difference between two arrays
-  console.log('\nFound the following items were NOT found in the ' + listName + ' list:');
-  for (i in difference) {
-    if (difference[i] == 'Appcelerator JIRA' || difference[i] == 'Report a bug' || difference[i] == 'Configure Release Notes' || difference[i] == 'Appcelerator JIRA') {
-    } else {
-      console.log(difference[i]);
+    if (program.rawArgs[i] == '--password' || program.rawArgs[i] == '-p') {
+      // ** password doesn't process some special characters like ', ", !, $....
+      arguments.pass = program.rawArgs[i + 1];
+    }
+    if (program.rawArgs[i] == '--jira' || program.rawArgs[i] == '-j') {
+      arguments.jira = program.rawArgs[i + 1];
+    }
+    if (program.rawArgs[i] == '--confluence' || program.rawArgs[i] == '-c') {
+      arguments.confluence = program.rawArgs[i + 1];
+    }
+    if (program.rawArgs[i] == '--delay' || program.rawArgs[i] == '-d') {
+      arguments.delay = parseInt(program.rawArgs[i + 1]);
     }
   }
-  console.log();
+
+  if (!arguments.delay) { // if the delay flag isn't set, assume 10 seconds
+    arguments.delay = 10000; // default the delay of the server response time to six seconds
+    console.log('Server response delay not set. Assuming ' + arguments.delay + ' millisecond delay.');
+  }
+
+  if (!arguments.user || !arguments.pass || !arguments.jira || !arguments.confluence) {
+    if (!arguments.user) { // user id is required
+      console.log('Username is required.');
+    }
+    if (!arguments.pass) { // password is required
+      console.log('Password is required.')
+    }
+    if (!arguments.jira) { // Jira's url is required
+      console.log('Jira\'s URL is required.')
+    }
+    if (!arguments.confluence) { // Confluence's url is required
+      console.log('Confluence\'s URL is required.')
+    }
+
+    process.exit(1);
+  } else {
+    console.log('Processing release note and Jira release board information.')
+
+    nightmare
+      .goto(arguments.confluence) // go to the required document. Jive will redirect to the login page but then load the requested document.
+      .type('#os_username', arguments.user)
+      .type('#os_password', arguments.pass)
+      .click('#loginButton')
+      .wait(arguments.delay) // wait for server to respond
+      .evaluate(confluenceSelector => { // grab html content from browser
+        return {
+          html: document.querySelector(confluenceSelector).innerHTML, // HTML element where the content is located
+          title: document.title // title of document
+        }
+      }, confluenceSelector)
+      .end() // close Electron browser
+      .then(obj => { // render html
+        $ = cheerio.load(obj.html.toString()); // read file in
+
+        $('ul li>a').each(function() { // find all links
+          if ($(this).html().indexOf('<s>') > -1) { // skip strike through links
+          } else if ($(this).attr('href').indexOf('jira.') > -1) { // if the link uses 'jira.' as part of it's URL, assume it's a Jira ticket link
+            confluenceList.push($(this).text().replace(/ /g,'')); // push the link into an array after removing any spaces from the text
+          }
+        });
+
+        var jiraArray = [];
+
+        request(arguments.jira, function(error, response, body) { // grab content from Jira release board (doesn't require a login since the project is public)
+          if (!error && response.statusCode == 200) {
+            $ = cheerio.load(body.toString());
+            $('section.aui-page-panel-content li a').each(function() { // find all links
+              if ($(this).text() != 'Configure Release Notes') {
+                jiraArray.push($(this).text())
+              }
+            });
+
+            let confluenceDiff = confluenceList.filter(x => !jiraArray.includes(x)).sort(); // find the differences between Confluence and Jira lists
+            let jiraDiff = jiraArray.filter(x => !confluenceList.includes(x)).sort(); // find the differences between Jira and Confluence lists
+
+            function uniq(a) { // remove duplicates in an array
+              var seen = {};
+              return a.filter(function(item) {
+                  return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+              });
+            }
+
+            if (confluenceDiff.length > 0) {
+              console.log('Listed in release note but not found in Jira:');
+              var confluenceUniq = uniq(confluenceDiff);
+              for (i in confluenceUniq) {
+                console.log(confluenceUniq[i]);
+              }
+            } else {
+              console.log('There was no differences between release note and Jira release board ticket lists.');
+            }
+            if (jiraDiff.length > 0) {
+              console.log('Listed in Jira but not found in release note:');
+              var jiraUniq = uniq(jiraDiff);
+              for (i in jiraUniq) {
+                console.log(jiraUniq[i]);
+              }
+            } else {
+              console.log('There was no difference between Jira release board and release note ticket lists')
+            }
+          } else {
+            console.log('Error! Cannot find "' + arguments.jira + '".');
+          }
+        });
+      })
+      .catch(error => { // catch any errors
+        console.error(error);
+      });
+  }
 }
+run();
