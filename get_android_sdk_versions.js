6de4af28950aa0bc307fa9670eacd04e7a9cff6f
Brian Immel <jawa9000@gmail.com>
2019-07-17 14:00:23 -0700

    Update get_android_sdk_versions.js
    Fixed usage message, updated version output messages, added a few more comments explaining their functionality, and added a sub-routine to detect if the minimum android/sdk version contains extra info.

--- get_android_sdk_versions.js
+++ get_android_sdk_versions.js
@@ -4,7 +4,7 @@
   Documentation: https://wiki.appcelerator.org/x/hxN1Aw
 */
 
-const Nightmare = require("nightmare");
+const Nightmare = require('nightmare');
 const cheerio = require('cheerio');
 const request = require('request');
 const nightmare = Nightmare({
@@ -12,15 +12,14 @@ const nightmare = Nightmare({
 });
 const program = require('commander');
 const selector = 'table.highlight'; // selector of where Github content can be found
+var arguments = {}; // object to hold the entered arguments
 
 program // set up flags
   .version('0.0.1')
-  .usage('-version <#_#> -d <milliseconds> -s <true/false>')
+  .usage('-release <#_#> -d <milliseconds>')
   .option('-r, --release', '*required* Version number of the release. For example: 8_0')
   .option('-d, --delay', 'Delay (in milliseconds) to wait for server response')
   .parse(process.argv);
-
-var arguments = {}; // object to hold the entered arguments
 
 for (var i = 0; i < program.rawArgs.length; i++) { // loop through the array of raw arguments to gather the entered arguments
   if (program.rawArgs[i] == '--release' || program.rawArgs[i] == '-r') {
@@ -37,7 +36,7 @@ if (!arguments.delay) { // if the delay flag isn't set, assume 10 seconds
 }
 
 if (!arguments.release) { // required flag not supplied
-  console.log('Release version number is required. For example 8_0.');
+  console.log('Release version number is required. For example: 8_0. Quitting attempt.');
 
   process.exit(1);
 } else {
@@ -52,39 +51,34 @@ if (!arguments.release) { // required flag not supplied
     }, selector)
     .end() // close Electron browser
     .then(obj => { // render html
-      console.log('Grabbing contents from ' + obj.title);
-      // console.log(obj.html);
-      var minSDKVersion = '';
-      var androidSdkMin = '';
-      var androidSdkMax = ''
+      console.log('Grabbing contents from "' + obj.title + '".');
 
-      $ = cheerio.load(obj.html.toString()); // read object in
+      var minSDKVersion = ''; // Minimum Android/SDK Version
+      var androidSdkMin = ''; // Min Target Android/SDK Version
+      var androidSdkMax = ''; // Max Target Android/SDK Version
+
+      $ = cheerio.load(obj.html.toString()); // read document object
 
       $('td').each(function() { // loop through the table to find the version info
         if ($(this).text().indexOf('minSDKVersion') > -1) { // min android
           minSDKVersion = $(this).text().split('": "')[1].replace('",', '');
-          console.log('Minimum Android/SDK Version: ' + minSDKVersion);
         }
+
         if ($(this).text().indexOf('"android sdk":') > -1) { // sdk min/max
           var text = $(this).text().split(' ');
           androidSdkMin = text[2].replace('">=', '').replace('.x', '');
           androidSdkMax = text[3].replace('<=', '').replace('.x",', '')
         }
-
       });
 
-      console.log('Min Target Android/SDK Version: ' + androidSdkMin);
-      console.log('Max Target Android/SDK Version:' + androidSdkMax);
-      // console.log('Google "android api level ' + minSDKVersion + '". The first result should show you the API version for the "Minimum Android/SDK Version" (#.#.x (API ' + minSDKVersion + '))');
-
       request('https://source.android.com/setup/start/build-numbers', function(error, response, body) { // figure out android info
-        console.log('Getting Android build numbers.')
-        if (!error && response.statusCode == 200) {
-          $ = cheerio.load(body.toString());
+        console.log('Getting Android build numbers from Android release note.\n')
+        if (!error && response.statusCode == 200) { // if the document loads without any errors, process it's data
+          $ = cheerio.load(body.toString()); // read document object
 
           var table = []; // array to hold table data
 
-          $('td').each(function() {
+          $('td').each(function() { // gather Android API data  based on the info found in the only table element
             table.push($(this).text());
           });
 
@@ -96,9 +90,15 @@ if (!arguments.release) { // required flag not supplied
               console.log('Max Target Android/SDK Version: ' + table[i - 1] + '.x (' + table[i].replace('level ', '') + ')');
             }
             if (table[i] == 'API level ' + minSDKVersion) {
-              console.log('Minimum Android/SDK Version: ' + table[i - 1] + '.x (' + table[i].replace('level ', '') + ')');
+              if (table[i - 1].indexOf(' - ') > -1) { // sometimes the version info will contain extra info (formatted like this: 4.4 - 4.4.x)
+                console.log('Minimum Android/SDK Version: ' + table[i - 1].slice(0, table[i - 1].indexOf(' - ')) + '.x (' + table[i].replace('level ', '') + ')');
+              } else {
+                console.log('Minimum Android/SDK Version: ' + table[i - 1] + '.x (' + table[i].replace('level ', '') + ')');
+              }
             }
           }
+
+          console.log('\nBe sure to cross reference this is info in https://github.com/appcelerator/titanium_mobile/blob/master/android/package.json (replacing master with the version) and https://developer.android.com/studio/releases/platforms.')
         }
       });
     });
