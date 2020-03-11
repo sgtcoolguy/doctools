785653c87c18ffec24543c5db07efe68277b6acf
Chris Williams <chris.a.williams@gmail.com>
2020-03-11 15:22:52 -0400

    feat(markdown): copy images, improve conversion of html to markdown

--- wiki/markdown.js
+++ wiki/markdown.js
@@ -1,31 +1,5 @@
-// TODO: Make a sensible HTML export -> markdown converter
-// that we could use to funnel our docs into https://github.com/Axway/axway-open-docs/blob/master/content/en/docs
-
-// There's this not so great npm package for converting that basically just shells out to pandoc:
-// https://github.com/EWhite613/Confluence-to-Github-Markdown/blob/master/Confluence-html-to-github-markdown.js
-
-// We could do better just rewriting that, and we need to pre-pend some metadata about each page to the markdown anyways
-// i.e.:
-// {
-//     "title":"Data storage for high availability",
-//     "linkTitle":"Data storage for HA",
-//     "weight":"3",
-//     "date":"2019-08-09",
-//     "description":"Understand where API Portal stores API Management data."
-// }
-
-// We could also try to use Turndown to do the conversion rather than relying on pandoc:
-// https://github.com/domchristie/turndown
-
- // Do we need to pre-prep the HTML?
- // - I think we need to strip the exporter footer...
- // - Do what we do in guides_parser to change absolute to relative links?
- // - Rewrite image reference paths/URLs
- // - Do we need to copy the redirect html files over top?
-
-
- /*
- * Copyright (c) 2015-2018 Appcelerator, Inc. All Rights Reserved.
+/*
+ * Copyright (c) 2020-Present Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License.
  *
  * Script to convert Wiki-exported content for JSDuck site.
@@ -38,12 +12,16 @@
 const fs = require('fs-extra');
 const path = require('path');
 const promisify = require('util').promisify;
+const xml2js = promisify(require('xml2js').parseString);
 const TurndownService = require('turndown');
 
 const manipulateHTMLContent = require('./htmlguides').manipulateHTMLContent;
 
-const cheerio = require('cheerio');
-const xml2js = promisify(require('xml2js').parseString);
+// Regexp/Patterns used to match link styles to rewrite them to work in docsy!
+const SAFE_ID_PATTERN = /^([\.\w]+)-section-src-\d+_safe-id-(\w+)/; // safe-id style anchor link: group 1 is page name, 2 is base64-encoded anchor name
+const ANCHOR_PATTERN = /^([\.\w]+)-section-src-\d+_([^-]+?)-(.+)/; // typical anchor link: group 1 is page name, 3 is anchor name
+const WIPE_ANCHOR_PATTERN = /^([\.\w]+)-section-src-\d+$/; // just drop the link?
+const DUMB_PATTERN = /^(.+?)-section-src-\d+_(.+)$/; // group 1 is the page name, group 2 is the full anchor name
 
 const WHITELIST = [
 	'https://wiki.appcelerator.org/display/community',
@@ -54,7 +32,6 @@ const WHITELIST = [
 // FIXME: Move to processCommandLineArgs, return in an object!
 let inputFile = null;
 let outputDir = null;
-let showEditButton = false;
 
 // TODO: Rewrite to be async!
 function parse(node, topicsDone = new Set()) {
@@ -122,9 +99,6 @@ function processCommandLineArgs() {
 					}
 					outputDir = path.resolve(cwd, process.argv[x]);
 					break;
-				case "--show_edit_button":
-					showEditButton = true;
-					break;
 				default:
 					console.warn(`unknown option: ${process.argv[x]}`);
 			}
@@ -148,9 +122,10 @@ function processCommandLineArgs() {
  * @param {object} entry 
  * @param {number} index
  * @param {string} outDir 
+ * @param {Map<string, string>} lookupTable
  */
-async function handleEntry(entry, index, outDir) {
-	console.log(`Converting ${entry} to markdown`);
+async function handleEntry(entry, index, outDir, lookupTable) {
+	// console.log(`Converting ${entry.title} to markdown`);
 	let outputName;
 	// TODO: if the entry has 'items' property, it's a parent! Need to recurse, and change filename to _index
 	if (entry.items) {
@@ -158,7 +133,7 @@ async function handleEntry(entry, index, outDir) {
 		outputName = '_index.md';
 		await fs.ensureDir(outDir);
 		// recurse
-		await Promise.all(entry.items.map((child, childIndex) => handleEntry(child, childIndex, outDir)));
+		await Promise.all(entry.items.map((child, childIndex) => handleEntry(child, childIndex, outDir, lookupTable)));
 	} else {
 		outputName = `${entry.name}.md`;
 	}
@@ -166,8 +141,6 @@ async function handleEntry(entry, index, outDir) {
 	const content = await fs.readFile(filepath, 'utf8');
 	// We need to alter the HTML like we do in htmlguides here: strip footer, etc.
 	const modified = await manipulateHTMLContent(content, filepath);
-	// FIXME: The code baked in to this method will alter links but may do so in a way we don't want!
-
 	// TODO: Can we extract the page title/strip it (so it isn't duplicated)?
 
 	// Convert the html -> markdown prepend the frontmatter
@@ -175,29 +148,156 @@ async function handleEntry(entry, index, outDir) {
 		title: entry.title,
 		weight: ((index + 1) * 10).toString() // Make the weight the (index + 1) * 10 as string
 	};
-	const turndownService = new TurndownService();
+	const turndownService = new TurndownService({ headingStyle: 'atx' });
+	// Convert the export HTML links to intra-docsy links if we can
+	turndownService.addRule('a', {
+		filter: function (node, options) {
+			return (
+				options.linkStyle === 'inlined' &&
+				node.nodeName === 'A' &&
+				node.getAttribute('href')
+			)
+		},
+		replacement: function (content, node) {
+			let href = node.getAttribute('href');
+			if (href.startsWith('#!/guide/')) {
+				href = wikiLinkToMarkdown(href, lookupTable);
+			}
+
+			const title = node.title ? ' "' + node.title + '"' : ''
+			return '[' + content + '](' + href + title + ')';
+		}
+	});
+	// Convert images to point at correct place!
+	// images/download/attachments/30083145/TabbedApplicationMain.png
+	// -> /Images/appc/download/attachments/30083145/TabbedApplicationMain.png
+	turndownService.addRule('img', {
+		filter: 'img',
+		replacement: function (content, node) {
+			let alt = node.alt || ''
+			let src = node.getAttribute('src') || ''
+			if (alt && src === alt) {
+				// the alt text is literally just the path to the image.
+				// Best we can do here is strip it to the base filename without the extension, I suppose.
+				alt = path.basename(alt, path.extname(alt));
+			}
+			if (src.startsWith('images/')) {
+				src = '/Images/appc' + src.substring(6); // 'images/...' -> '/Images/appc/...'
+			}
+			const title = node.title || ''
+			const titlePart = title ? ' "' + title + '"' : ''
+			return src ? '![' + alt + ']' + '(' + src + titlePart + ')' : ''
+		}
+	});
 	const markdown = turndownService.turndown(modified);
 	const converted = `${JSON.stringify(frontmatter)}\n\n${markdown}`;
 	return fs.writeFile(path.join(outDir, outputName), converted);
 }
 
-async function convertTOC(inputFile, outputDir) {
+/**
+ * @param {string} href 
+ * @param {Map<string, String>} lookupTable 
+ * @returns {string}
+ */
+function wikiLinkToMarkdown(href, lookupTable) {
+	let anchor;
+	const endPath = href.replace('#!/guide/', '');
+	let pageName = endPath;
+	const dumbMatch = endPath.match(DUMB_PATTERN);
+	if (dumbMatch) {
+		pageName = dumbMatch[1];
+		anchor = dumbMatch[2];
+		if (anchor.startsWith('safe-id-')) {
+			const buff = Buffer.from(anchor.substring(8), 'base64');
+			anchor = buff.toString('ascii');
+			// TODO: Url encode?
+		}
+		// ok now we need to drop the pagename from prefix of anchor!
+		const anchorPageName = pageName.replace(/_/g, '');
+		if (anchor.startsWith(anchorPageName)) {
+			anchor = anchor.substring(anchorPageName.length + 1); // drop the page name prefix plus the trailing '-'
+		}
+		// FIXME: I think the anchor links are busted anyways because they combined all spaces
+		// i.e. #Fixedissues should point to the heading "Fixed Issues" and should instead be #fixed-issues
+		// so how the hell do we fix this? Serach the page for all headings and remove spaces and find match and then convert?
+		// What about cross-page anchor links?!
+	}
+
+	// console.log(`path: ${endPath}\npage: ${pageName}\nanchor: ${anchor}`);
+
+	if (!pageName) {
+		// couldn't extract the page name, so return the link as it was
+		return href;
+	}
+
+	// TODO: if link is pointing to itself, drop the page name and just use the anchor!
+
+	const docPath = lookupTable.get(pageName);
+	if (!docPath) {
+		console.warn(`Unable to find path of page: ${pageName}, from: ${href}`);
+		return href;
+	}
+	let result = `${docPath}`;
+	if (anchor) {
+		result += `#${anchor}`;
+	}
+	// console.log(`${href} -> ${result}`);
+	return result;
+}
+
+/**
+ * @param {string} inputFile toc.xml file exported from wiki
+ * @param {string} outputDir root directory we're assembling our docsy content
+ * @returns {Promise<void>}
+ */
+async function convertHTMLFiles(inputFile, outputDir) {
 	const contents = await fs.readFile(inputFile, 'utf8');
 	const result = await xml2js(contents);
 	const toc = parse(result.toc.topic);
-	// TODO: instead of writing a guides.json file, loop through the entries and convert each to markdown with the JSON header!
-	// What we want to do is use the structure here to build the structure of the dir tree
-	// if an entry is a parent, it should become a folder and it's content goes to _index.md
-	// If it's a leaf, the file shoudl be named after the "name" value here, the frontmatter of the content shoudl have title match "title" here
-	// "weight" in frontmatter should be = (the order/index of the entries in the array + 1) * 10
-	// toc should be an arry here
-	// handle each entry separately
-	return Promise.all(toc.map((entry, index) => handleEntry(entry, index, outputDir)));
+
+	const lookupTable = new Map();
+	generateLookupTable('/docs/appc/', toc, lookupTable);
+
+	const docsDir = path.join(outputDir, 'content/en/docs/appc');
+	await fs.ensureDir(docsDir);
+	return Promise.all(toc.map((entry, index) => handleEntry(entry, index, docsDir, lookupTable)));
 }
 
+/**
+ * Copy the images over to where we'd expect: htmlguides/images -> outputDir/static/Images/appc
+ * @param {string} outputDir root directory we're assembling our docsy content
+ * @returns {Promise<void>}
+ */
+async function copyImages(outputDir) {
+	const imagesDir = path.join(outputDir, 'static/Images/appc');
+	return fs.copy(path.join(__dirname, 'htmlguides/images'), imagesDir);
+}
+
+/**
+ * @param {string} prefix
+ * @param {object[]} entries 
+ * @param {Map<string, string>} lookupTable 
+ */
+function generateLookupTable(prefix, entries, lookupTable) {
+	for (const entry of entries) {
+		const generatedPath = `${prefix}${entry.name}/`
+		lookupTable.set(entry.name, generatedPath);
+		if (entry.items) {
+			// recurse inside items!
+			generateLookupTable(generatedPath, entry.items, lookupTable);
+		}
+	}
+}
+
+/**
+ * @returns {Promise<void>}
+ */
 async function main() {
 	processCommandLineArgs();
-	return convertTOC(inputFile, outputDir);
+	return Promise.all([
+		convertHTMLFiles(inputFile, outputDir),
+		copyImages(outputDir)
+	]);
 }
 
 main().then(() => process.exit(0)).catch(err => {
