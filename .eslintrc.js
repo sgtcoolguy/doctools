87a2fdeadecb1a4ecc08a4a92eace281637de99a
Chris Williams <chris.a.williams@gmail.com>
2020-05-04 14:29:37 -0400

    style(js): add eslint config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,7 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
+    "extends": [ "axway/env-node"],
     "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
+      "ecmaVersion": 2018,
+      "sourceType": "script"
     }
 };
